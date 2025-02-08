003af5d5ad1075c1566c6a587c95c70d55952787
ALVARO MIGUEL OBIES GARCIA <100472136@alumnos.uc3m.es>
2025-02-08 06:53:47 +0100

    added ip as argument

--- send.py
+++ send.py
@@ -4,12 +4,12 @@ import socket
 BUFSIZE = 1024
 
 def main(argc, argv):
-    if argc != 2:
-        print("Usage ./execute.py <path>")
+    if argc != 3:
+        print("Usage ./execute.py <IP> <path>")
         return
 
     try:
-        with open(argv[1], "r") as file:
+        with open(argv[2], "r") as file:
             code = file.read()
     except FileNotFoundError:
         print("File not found")
@@ -17,7 +17,7 @@ def main(argc, argv):
 
     try:
         connectionSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        connectionSocket.connect(("localhost", 12000))
+        connectionSocket.connect((argv[1], 12000))
         connectionSocket.send(code.encode())
         response = connectionSocket.recv(BUFSIZE).decode()
         print(response)
